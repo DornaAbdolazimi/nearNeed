74918c55885baf5c96d5e9cd04960988e87e8626
narges shadab <nargesshadab@nargess-MacBook-Pro.local>
2015-08-28 12:20:15 +0430

    fix navbar for profile

---
+++ Untitled.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("Hello, narges shadab. Welcome to CodeRunner! ");
+}
--- dist/js/edit-profile.js
+++ dist/js/edit-profile.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
- 	$('#container').on("scroll", function () {
+	$(".navbar-brand").css({"font-size": "30px"});
+	$(".navbar").css({"background-color": "#204056"}); 
+	$(".container-fluid").css({"padding-top": "5px"}); 
+	$("#section2").css({"padding-left": "200px"});
+
+$('#container').on("scroll", function () {
  	console.log('here');
  	var st = $('#container').scrollTop ();
  	console.log(st);
- 	st = st < 300 ? st : 300;
- 	$('#profile-pic').css('top', 320 - st);
- 	st = st < 80 ? st : 80;
-	$('#profile-pic').css('height', 200 * (1 - st / 150));
-	 $('#profile-pic').css('width', 200 * (1 - st / 150));
+ 	st = st < 350 ? st : 350;
+ 	$('#profile-pic').css('top', 360 - st);
+ 	st = st < 180 ? st : 180;
+	$('#profile-pic').css('height', 200 * (1 - st / 350));
+	$('#profile-pic').css('width', 200 * (1 - st / 350));
 	});
+
+//$(window).on("scroll", function(){
+//	if ($(this).scrollTop()>1) {
+//		$('.navbar')addClass("sticky");
+//	}else{
+//		$('.navbar').removeClass("sticky");
+//	}
+//});
+
 });
