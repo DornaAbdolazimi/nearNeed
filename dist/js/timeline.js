fdbc3658f03b9a20ca00de1b8a14ae68a8a3eb53
alimfaraji <hoco.hc@gmail.com>
2015-08-30 01:04:06 +0430

    add price range and also category button

--- dist/js/timeline.js
+++ dist/js/timeline.js
@@ -22,4 +22,55 @@ $("#theme4").click(function(){$(".nav-button").css({"background-color": "#806BAE
 $("#theme4").click(function(){$(".gly").css({"color": "#806BAE"});});
 $("#theme5").click(function(){$(".nav-button").css({"background-color": "#E3695C"});});
 $("#theme5").click(function(){$(".gly").css({"color": "#E3695C"});});
+    var options = [];
+
+    $( '.dropdown-menu a' ).on( 'click', function( event ) {
+
+        var $target = $( event.currentTarget ),
+            val = $target.attr( 'data-value' ),
+            $inp = $target.find( 'input' ),
+            idx;
+
+        if ( ( idx = options.indexOf( val ) ) > -1 ) {
+            options.splice( idx, 1 );
+            setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+        } else {
+            options.push( val );
+            setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+        }
+
+        $( event.target ).blur();
+
+        console.log( options );
+        return false;
+    });
+
+    var nonLinearSlider = document.getElementById('nonlinear');
+
+    noUiSlider.create(nonLinearSlider, {
+        connect: true,
+        start: [ 0, 100.000 ],
+        step: 0.001,
+        range: {
+            'min':  0 ,
+            '50%': 10,
+            '80%': 40,
+            'max': 100.000
+        },
+        format: wNumb({
+            decimals: 3,
+            mark: ' ',
+            thousand: '.',
+            postfix: 'تومان',
+        })
+    });
+
+    var snapValues = [
+        document.getElementById('slider-snap-value-lower'),
+        document.getElementById('slider-snap-value-upper')
+    ];
+
+    nonLinearSlider.noUiSlider.on('update', function( values, handle ) {
+        snapValues[handle].innerHTML = values[handle];
+    });
 });
